c87f46c447f8b5a70afd152739861fecb953f2e2
jayzane <jayzane@163.com>
2019-05-06 00:32:59 +0800

    UPDATE finish 257

--- search/257_binary_tree_paths.py
+++ search/257_binary_tree_paths.py
@@ -1,6 +1,6 @@
 from typing import *
 
-import common
+from common import init_data
 
 
 # Definition for a binary tree node.
@@ -36,17 +36,60 @@ class Solution:
         pass
 
 
-def binary_tree_paths(root: TreeNode) -> List[str]:
+def binary_tree_paths_back(root: TreeNode) -> List[str]:
+    """
+    :param root:
+    :return:
+    >>> node_1 = init_data.init_tree([1,2,3,None,5], 0)
+    >>> binary_tree_paths_back(node_1)
+    ['1->2->5', '1->3']
+    """
 
-    def search_path(paths: List[List[int]], current_path: List[int], node: TreeNode, visited: List[TreeNode]) -> None:
+    def search_path(paths: List[List[int]], current_path: List[int], node: TreeNode) -> None:
         if node is None:
+            return
+        current_path.append(node.val)
+        if node.left is None and node.right is None:
             paths.append(current_path)
-        paths.append(node.val)
-        search_path(paths, node.left)
-        search_path(paths, node.right)
+            return
+        tmp: List[int] = current_path[:]
+        search_path(paths, current_path, node.left)
+        current_path = tmp
+        search_path(paths, current_path, node.right)
 
-    search_path([], root)
+    res_paths: List[List[int]] = []
 
+    search_path(res_paths, [], root)
+    res: List[str] = list(map(lambda x: '->'.join(map(lambda y: str(y), x)), res_paths))
+    return res
+
+
+def binary_tree_paths(root):
+    """
+    >>> node_1 = init_data.init_tree([1,2,3,None,5], 0)
+    >>> binary_tree_paths(node_1)
+    ['1->2->5', '1->3']
+    """
+
+    def join_paths(node: TreeNode, left_paths: List[List[int]]) -> List[List[int]]:
+        return [[node.val] + p for p in left_paths]
+
+    def search_paths(node: TreeNode) -> List[List[int]]:
+        if not node:
+            return []
+        elif not node.left and not node.right:
+            return [[node.val]]
+        elif not node.left:
+            return join_paths(node, search_paths(node.right))
+        elif not node.right:
+            return join_paths(node, search_paths(node.left))
+        left_paths = search_paths(node.left)
+        right_paths = search_paths(node.right)
+        return join_paths(node, left_paths + right_paths)
+
+    paths: List[List[int]] = search_paths(root)
+    res: List[str] = list(map(lambda x: '->'.join(map(lambda y: str(y), x)), paths))
+    return res
 
 
 if __name__ == '__main__':
